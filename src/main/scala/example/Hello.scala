f7f710dd28ed478987d40d8e098c44f208300cf0
Shuichi Tsukamoto <shtsukam@microsoft.com>
2017-06-26 18:24:54 +0900

    update README.markdown

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -14,7 +14,7 @@ import org.jclouds.Constants
 import org.gaul.s3proxy.S3Proxy
 
 object Hello extends Greeting with App {
-  println("hello")
+  println(greeting)
   s3ProxyStart
   def s3ProxyStart() : Unit = {
     val path = FileSystems.getDefault().getPath("conf", "s3proxy.conf")
@@ -26,22 +26,22 @@ object Hello extends Greeting with App {
         case Some(identity) => Option(properties.getProperty(Constants.PROPERTY_CREDENTIAL)) match {
           case Some(credential) => {
             val context = ContextBuilder.newBuilder(provider).
-                          credentials(identity, credential).
-                          overrides(properties).build(classOf[BlobStoreContext])
+            credentials(identity, credential).
+            overrides(properties).build(classOf[BlobStoreContext])
             val s3Proxy = S3Proxy.builder().blobStore(context.getBlobStore()).
-                          endpoint(URI.create("http://127.0.0.1:8080")).build()
-            s3Proxy.start()
-            while (!s3Proxy.getState().equals(AbstractLifeCycle.STARTED)) {
-               Thread.sleep(1)
-               println("Not Start!")
-            }
-            println("Start!")
+            endpoint(URI.create("http://127.0.0.1:8080")).build()
+              s3Proxy.start()
+              while (!s3Proxy.getState().equals(AbstractLifeCycle.STARTED)) {
+                Thread.sleep(1)
+                println("Not Start!")
+              }
+              println("Start!")
           }
           case None => println("$Constants.PROPERTY_CREDENTIAL is not exists.")
         }
-        case None => println("Constants.PROPERTY_IDENTITY is not exists.")
+          case None => println("Constants.PROPERTY_IDENTITY is not exists.")
       }
-      case None => println("$Constants.PROPERTY_PROVIDER is not exists")
+          case None => println("$Constants.PROPERTY_PROVIDER is not exists")
     }
   }
 }
